2739cfdbea271c7d069e44ba5da05040db5f1cb8
Topaz24 <topaz2490@gmail.com>
2025-04-07 21:26:42 +0300

    added the test.py file

---
+++ test.py
@@ -0,0 +1,10 @@
+# This code draws a smiley face using '*' characters
+
+print("   *****   ")
+print("  *     *  ")
+print(" *  O O  * ")
+print(" *   ^   * ")
+print(" *  \\_/  * ")
+print("  *     *  ")
+print("   *****   ")
+
